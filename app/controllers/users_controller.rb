cde6458a57062b18cd52b3593f725d2395b1d8d5
Luke Bertram <lukebertram@gmail.com>
2018-04-03 11:36:13 -0700

    add gravatar image and sidebar to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+
+  def index   # GET users_path
+
+  end
+
+  def show    # GET user_path(user)
+    @user = User.find(params[:id])
+  end
+
+  def new     # GET new_user_path
+  end
+
+  def create  # POST users_path
+
+  end
+
+  def edit    # GET edit_user_path(user)
+
+  end
+
+  def update  # PATCH user_path(user)
+
+  end
+
+  def destroy # DELETE user_path(user)
+
   end
 end
