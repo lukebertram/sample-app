04cccf1257a712c5ec759f30ae9ac3391a3a5d9a
Luke Bertram <lukebertram@gmail.com>
2018-04-06 11:52:59 -0700

    display success flash after successful new user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   def create  # POST users_path
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcom to the Sample App!"
+      redirect_to @user # == redirect_to user_url(@user)
     else
       render 'new'
     end
