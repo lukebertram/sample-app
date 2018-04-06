0c17c0a9475f25c5e2068cdee0b58dcd5f661358
Luke Bertram <lukebertram@gmail.com>
2018-04-06 10:31:29 -0700

    add strong parameters method for new user creation hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,16 @@ class UsersController < ApplicationController
   end
 
   def new     # GET new_user_path
+    @user = User.new
   end
 
   def create  # POST users_path
-
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
 
   def edit    # GET edit_user_path(user)
@@ -26,4 +32,11 @@ class UsersController < ApplicationController
   def destroy # DELETE user_path(user)
 
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
