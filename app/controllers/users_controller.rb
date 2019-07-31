9acfdbac3d2cbd382fdf9de49112a36c901918d2
Luke Bertram <lukebertram@gmail.com>
2019-07-30 23:32:27 -0700

    complete section 10.2.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def index   # GET users_path
 
   end
@@ -24,11 +27,15 @@ class UsersController < ApplicationController
   end
 
   def edit    # GET edit_user_path(user)
-
   end
 
   def update  # PATCH user_path(user)
-
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy # DELETE user_path(user)
@@ -40,5 +47,21 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
