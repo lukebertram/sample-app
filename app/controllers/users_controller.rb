65f124e543e82f35e8fd1117b74ddbfb24041698
Luke Bertram <lukebertram@gmail.com>
2019-07-31 12:05:50 -0700

    add pagination to users index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
   def index   # GET users_path
-
+    # todo: adjust this method
+    @users = User.paginate(page: params[:page])
   end
 
   def show    # GET user_path(user)
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:luke)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,9 +12,18 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    # test navigation links for logged-in user
+    log_in_as(@user)
+    assert is_logged_in?
+    assert_redirected_to @user
+    # why does the following assert fail?
+    # assert_select "a[href=?]", edit_user_path(@user)
   end
 end
